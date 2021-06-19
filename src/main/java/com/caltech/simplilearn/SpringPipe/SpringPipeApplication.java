76d70bb3020b4ae3ebd1a2eab1afeee2eb7d1e8c
student <nagendra@skillvelocity.in>
2021-06-19 16:34:12 +0000

    Cool Maven Spring project

---
+++ src/main/java/com/caltech/simplilearn/SpringPipe/SpringPipeApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.simplilearn.SpringPipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPipeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/simplilearn/SpringPipe/SpringPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.simplilearn.SpringPipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
